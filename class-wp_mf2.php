18d9f462c91796f7a6f8bdaab38ff534f3d8748e
David Shanske <david@shanske.com>
2015-05-09 19:20:25 -0400

    WordPress Coding Standards and Enhancements

--- class-wp_mf2.php
+++ class-wp_mf2.php
@@ -1,57 +1,123 @@
 <?php
+// Microformats 2 Support
 
-add_action('init', array('wp_mf2', 'init'), 12);
-
+add_action( 'init' , array( 'wp_mf2' , 'init' ), 12 );
 
 class wp_mf2 {
-  /**
-   * Initialize the plugin.
-   */
-  public static function init() {
-    if( current_theme_supports('microformats2') ) {
-      add_filter( 'post_class', array('wp_mf2', 'post_class'), 99 );
-      add_filter( 'body_class', array('wp_mf2', 'body_class') );
-      add_filter( 'comment_class', array('wp_mf2', 'comment_class') );
-      add_filter( 'get_comment_author_link', array('wp_mf2', 'get_comment_author_link') );
- //     add_filter( 'get_avatar', array('wp_mf2', 'get_avatar') );
-    // Enqueue Scripts as Needed
-    add_action('wp_enqueue_scripts', array('wp_mf2', 'enqueue_scripts') );
+	/**
+ 	* Adds class functions to the various filters.
+ 	* @since 0.1.0
+ 	*/
+	public static function init() {
+			add_filter( 'post_class' , array( 'wp_mf2' , 'post_class' ), 99 );
+			add_filter( 'body_class' , array( 'wp_mf2' , 'body_class' ) );
+			add_filter( 'comment_class' , array( 'wp_mf2' , 'comment_class' ) );
+			add_filter( 'get_comment_author_link' , array( 'wp_mf2' , 'get_comment_author_link' ) );
+			add_filter('pre_get_avatar_data', array( 'wp_mf2', 'pre_get_avatar_data' ), 11 , 5 );
+			// add_filter( 'get_avatar' , array( 'wp_mf2' , 'get_avatar' ) );
+			// Enqueue Scripts as Needed
+			// add_action('wp_enqueue_scripts' , array( 'wp_mf2' , 'enqueue_scripts' ) );
+  }
+
+/**
+ * Filters the array of classes for the post div.
+ *
+ * Adds microformats2 classes for the post div.
+ *
+ * @since 0.1.0
+ * @param type string|array $class One or more classes to add to the class list.
+ * @return type array Array of classes.
+ */
+	public static function post_class($classes) {
+		$classes = array_diff( $classes, array( 'hentry' ) );
+		if ( ! is_singular() ) {
+			// Adds a class for microformats v2
+			$classes[] = 'h-entry';
+			// add hentry to the same tag as h-entry
+     	$classes[] = 'hentry';
+			// adds microformats 2 activity-stream support
+			// for pages
+			if ( get_post_type() == "page" ) {
+			$classes[] = "h-as-page";
+			} 
+		}
+		return $classes;
+  }
+
+/**
+ * Filters the array of classes for the body div.
+ *
+ * Adds microformats2 classes for the body div.
+ *
+ * @since 0.1.0
+ * @param type string|array $class One or more classes to add to the class list.
+ * @return type array Array of classes.
+ */
+	public static function body_class($classes) {
+		if (!is_singular()) {
+			$classes[] = "hfeed";
+			$classes[] = "h-feed";
+			$classes[] = "feed";
+		} 
+		else {
+			// Adds a class for microformats v2
+			$classes[] = 'h-entry';
+			// add hentry to the same tag as h-entry
+			$classes[] = 'hentry';
+		}
+		return $classes;
+	}
+
+/**
+ * Filters the array of classes for the comments div.
+ *
+ * Adds microformats2 classes for the comments div.
+ *
+ * @since 0.1.0
+ * @param type string|array $class One or more classes to add to the class list.
+ * @return type array Array of classes.
+ */
+	public static function comment_class($classes) {
+  	$classes[] = "p-comment";
+  	$classes[] = "h-entry";
+    return $classes;
+  }
+
+/**
+ * Filter the comment author's link for display.
+ *
+ * @since 0.1.0
+ *
+ * @param string $return     The HTML-formatted comment author link.
+ *                           Empty for an invalid URL.
+ * @param string $author     The comment author's username.
+ * @param int    $comment_ID The comment ID.
+ */
+  public static function get_comment_author_link( $return, $author, $comment_ID ) {
+		$url = get_comment_author_url( $comment_ID );
+		if ( empty( $url ) || 'http://' == $url )
+			$return = $author;
+		else
+			$return = "<a href='$url' rel='external nofollow' class='url u-url'>$author</a>";
+		return $return;
+  }
+
+/**
+ * adds classes to get_avatar
+ *
+ * @param array $args Arguments passed to get_avatar_data(), after processing.
+ * @param int|string|object $id_or_email A user ID, email address, or comment object
+ * @return array $args
+ */
+	public static function pre_get_avatar_data($args, $id_or_email) {
+    if(!isset($args['class']) ) {
+      $args['class']=array('u-photo');
     }
-  }
-  public static function post_class($classes) {
-    $classes = array_diff($classes, array('hentry'));
-    if (!is_singular()) {
-     // Adds a class for microformats v2
-     $classes[] = 'h-entry';
-     // add hentry to the same tag as h-entry
-     $classes[] = 'hentry';
-     // adds microformats 2 activity-stream support
-     // for pages
-     if ( get_post_type() == "page" ) {
-      $classes[] = "h-as-page";
-     } 
-   }
-  return $classes;
-  }
-  public static function body_class($classes) {
-    if (!is_singular()) {
-      $classes[] = "hfeed";
-      $classes[] = "h-feed";
-      $classes[] = "feed";
-    } 
     else {
-      // Adds a class for microformats v2
-      $classes[] = 'h-entry';
-      // add hentry to the same tag as h-entry
-      $classes[] = 'hentry';
+      $args['class'][]='u-photo';
     }
-    return $classes;
-  }
-  public static function comment_class($classes) {
-    return $classes;
-  }
-  public static function get_comment_author_link($link) {
-    return $link;
+		$args['class']=array_unique($args['class']);  
+    return $args;
   }
   
   public static function enqueue_scripts() {
--- semantics.php
+++ semantics.php
@@ -2,49 +2,77 @@
 // Purpose of these functions is to further enhance control of semantic
 // markup in WordPress
 
+/**
+ * Gets attributes.
+ *
+ * Retrieves attributes associated with what a particular tag surrounds.
+ *
+ * @since 0.1.0
+ * @param type string $id The type of tag, for example, body or post.
+ * @param type array $class Optional. Array of attributes
+ * @return type Array of attributes.
+ */
 if (!function_exists('get_attributes')) {
-  function get_attributes($id, $class = null) {
-    switch($id) {
-      case 'body':
-        $classes['class'] = get_body_class(;
-        if (!is_singular()) {
-          $classes['class'] = 'hfeed';
-        }
-        break;
-      case 'post':
-        $classes['class'] = get_post_class();
-        break;
-      case 'site-title':
-        break;
-      case 'site-description':
-        break;
-      case 'content':
-        $classes['class'] = 'entry-content';
-        break;
-      case 'published':
-        $classes['class'] = 'published';
-        break;
-      case 'updated':
-        $classes['class'] = 'updated';
-        break;
-    }    
-    array_merge($classes, array($class) );
-    $classes = apply_filters( "attributes", $classes, $id );
-    $classes = apply_filters( "attributes_{$id}", $classes, $id );
-    return $classes;
-  }
+	function get_attributes($id, $class = null) {
+		switch($id) {
+			case 'body':
+				$classes['class'] = get_body_class(;
+				if (!is_singular()) {
+					$classes['class'] = 'hfeed';
+				}
+			break;
+			case 'post':
+				$classes['class'] = get_post_class();
+				break;
+			case 'site-title':
+				break;
+			case 'site-description':
+				break;
+			case 'content':
+				$classes['class'] = 'entry-content';
+			break;
+			case 'published':
+				$classes['class'] = 'published';
+				break;
+			case 'updated':
+				$classes['class'] = 'updated';
+			break;
+		}    
+		array_merge($classes, array($class) );
+		/**
+		 * Filter the list of attributes.
+		 *
+		 * @since 0.1.0
+		 *
+		 * @param array  $classes An array of attributes.
+		 * @param string $id The type of tag
+		 */
+		$classes = apply_filters( "attributes", $classes, $id );
+		$classes = apply_filters( "attributes_{$id}", $classes);
+		return $classes;
+	}
 }
 
+/**
+ * Echoes attributes.
+ *
+ * Echos the output of get_attributes attributes
+ *
+ * @since 0.1.0
+ * @param type string $id The type of tag, for example, body or post.
+ * @param type array $class Optional. Array of attributes
+ */
+
 if (!function_exists('the_attributes')) {
-  function the_attributes($id, $class = null) {
-    $classes = get_the_attributes($id, $class);
-    if (!$classes) {
-      return;
-    }
-    foreach ( $classes as $key => $value ) {
-      echo ' ' . esc_attr( $key ) . '="' . esc_attr( join( ' ', array_unique($value) ) ) . '"';
-    }
-  }
+	function the_attributes($id, $class = null) {
+		$classes = get_the_attributes($id, $class);
+		if (!$classes) {
+			return;
+		}
+		foreach ( $classes as $key => $value ) {
+			echo ' ' . esc_attr( $key ) . '="' . esc_attr( join( ' ', array_unique($value) ) ) . '"';
+		}
+	}
 }
 
 
